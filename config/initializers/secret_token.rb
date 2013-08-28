64943af1529cebbe3108bc52c80b15ca672eff62
Jamie Franklin <jamie@intertitles.com>
2013-08-28 23:08:32 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inkbook::Application.config.secret_key_base = '8993ee23124f8e2e73a2ec843d0db62bc989494a43fd40ea8ddc0169ec24af7c735fb01eab6ebf88e5e9761299066dc583b409bf51bbc6664b957dcb81d94e61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inkbook::Application.config.session_store :cookie_store, key: '_inkbook_session'
